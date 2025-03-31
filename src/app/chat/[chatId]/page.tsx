ed747dbd9d963ccd26788035d550190fc6aedcd9
DryTurtle391 <binayakway@gmail.com>
2025-03-31 18:51:05 +0530

    chatsidebar and pdf viewer

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,3 +1,5 @@
+import ChatSideBar from "@/components/ChatSideBar";
+import PDFViewer from "@/components/PDFViewer";
 import { db } from "@/lib/db";
 import { chats } from "@/lib/db/schema";
 import { auth } from "@clerk/nextjs/server";
@@ -25,14 +27,18 @@ const ChatPage = async ({ params: { chatId } }: Props) => {
   if (!_chats.find((chat) => chat.id === parseInt(chatId))) {
     return redirect("/");
   }
+
+  const currentChat = _chats.find((chat) => chat.id == parseInt(chatId));
   return (
     <div className="flex max-h-screen overflow-scroll">
       <div className="flex w-full max-h-screen overflow-scroll">
         {/* chat sidebar */}
-        <div className="flex-[1] max-w-xs">{/* <ChatSideBar */}</div>
+        <div className="flex-[1] max-w-xs">
+          {<ChatSideBar chats={_chats} chatId={parseInt(chatId)} />}
+        </div>
         {/* pdf viewer */}
         <div className="flex-[5] max-h-screen p-4 overflow-scroll">
-          {/* PDF Viewer */}
+          <PDFViewer pdfUrl={currentChat?.pdfUrl || ""} />
         </div>
         {/* chat component */}
         <div className="flex-[3] border-1-4 border-1-slate-200">
